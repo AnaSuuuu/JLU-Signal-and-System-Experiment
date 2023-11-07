635fbf4d9ee78ae438f41475811769656eb1952d
Ana_Su <1774695537@qq.com>
2023-11-07 19:16:39 +0800

    updated on 23/11/7

--- lab03/drawing1.py
+++ lab03/drawing1.py
@@ -10,38 +10,38 @@ plt.rcParams['axes.unicode_minus'] = False
  
 # 这三个表示就跟名字一样，标识作用
 plt.title("频谱图", fontdict=font2)
-plt.xlabel("角频率ω", fontdict=font2, loc = "right")
-plt.ylabel("谐波电压Un", fontdict=font2, loc = "top")
+plt.xlabel("频率f", fontdict=font2, loc = "right")
+plt.ylabel("幅频Nd(ω)", fontdict=font2, loc = "top")
  
 # 这个前两参数表示横坐标的开始和结尾，第三个参数表示你要分成多少份
-x = np.linspace(0, 50, 10000)
+x = np.linspace(0, 45, 10000)
 # y = np.linspace(0, 1, 100)
  
 # 下面是节点个状态信息，一定要使用numpy自带的array，不然会出错
-node1State = np.array([0.57, 0.45, 0.30, 0.14, 0.03, 0.08, 0.13, 0.11, 0.09, 0.01])
-times = np.array([10, 20, 30, 40, 50, 120, 140, 160, 180, 200])
+node1State = np.array([0.881048873, 0.794328235, 0.691830971, 0.595662144, 0.530884444, 0.45708819, 0.489778819, 0.375837404, 0.28840315])
+times = np.array([5, 10, 15, 20, 25, 30, 35, 40, 45])
  
 # 这个就是最重要的平滑操作了，要是不使用这个操作的话，画出来的就是点和点之间的直线
 model0 = make_interp_spline(times, node1State)
 ys0 = model0(x)
  
 # 给每条线设定颜色，和添加label，linestyle表示你要使用曲线的样式，有多少种网上有
-# plt.plot(x, ys0)# , linestyle='-.'
+plt.plot(x, ys0 , linestyle='-.')#
  
 # 给定点的横纵坐标
-x_points = [20, 40, 60, 80, 100, 120, 140, 160, 180, 200]
-y_points = [0.57, 0.45, 0.30, 0.14, 0.03, 0.08, 0.13, 0.11, 0.09, 0.01]
+x_points = [5, 10, 15, 20, 25, 30, 35, 40, 45]
+y_points = [0.881048873, 0.794328235, 0.691830971, 0.595662144, 0.530884444, 0.45708819, 0.489778819, 0.375837404, 0.28840315]
 
 # 创建垂线的长度
 line_lengths = [0.57, 0.45, 0.30, 0.14, 0.03, 0.08, 0.13, 0.11, 0.09, 0.01]
 
-# plt.scatter(x_points, y_points, color='#1579ef')
+plt.scatter(x_points, y_points, color='#1579ef')
 # 画垂线
 # for i in range(len(x_points)):
 #    plt.vlines(x = x_points[i], ymin = 0, ymax = line_lengths[i], color='red', linestyle='--')
 
 # 理想情况
-x0 = np.arange(0, 50, 0.1)
+x0 = np.arange(0, 45, 0.1)
 y0 = 300 / np.sqrt(300 * 300 + np.pi * np.pi * x0 * x0 * 6.8 * 6.8 )
 # y0 = abs(80 / (x0 * np.pi) * np.sin(x0 * np.pi / 100))
 plt.plot(x0,y0)
--- lab03/drawing2.py
+++ lab03/drawing2.py
@@ -10,40 +10,40 @@ plt.rcParams['axes.unicode_minus'] = False
  
 # 这三个表示就跟名字一样，标识作用
 plt.title("频谱图", fontdict=font2)
-plt.xlabel("角频率ω", fontdict=font2, loc = "right")
-plt.ylabel("谐波电压Un", fontdict=font2, loc = "top")
+plt.xlabel("频率f", fontdict=font2, loc = "right")
+plt.ylabel("幅频Nd(ω)", fontdict=font2, loc = "top")
  
 # 这个前两参数表示横坐标的开始和结尾，第三个参数表示你要分成多少份
-x = np.linspace(0, 150, 10000)
+x = np.linspace(0, 50, 10000)
 y = np.linspace(0, 1, 100)
  
 # 下面是节点个状态信息，一定要使用numpy自带的array，不然会出错
-node1State = np.array([0.56, 0.45, 0.30, 0.14, 0.002, 0.09, 0.13, 0.11, 0.054, 0.01, 0.045, 0.07, 0.06, 0.02, 0.002])
-times = np.array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150])
+node1State = np.array([0.21, 0.39, 0.51, 0.61, 0.68, 0.72, 0.86, 0.78, 0.83, 1.1])
+times = np.array([5, 10, 15, 20, 25, 30, 35, 40, 45, 50])
  
 # 这个就是最重要的平滑操作了，要是不使用这个操作的话，画出来的就是点和点之间的直线
 model0 = make_interp_spline(times, node1State)
 ys0 = model0(x)
  
 # 给每条线设定颜色，和添加label，linestyle表示你要使用曲线的样式，有多少种网上有
-# plt.plot(x, ys0, linestyle='-.')#
+plt.plot(x, ys0, linestyle='-.')#
 
  
 # 给定点的横纵坐标
-x_points = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150]
-y_points = [0.56, 0.45, 0.30, 0.14, 0.002, 0.09, 0.13, 0.11, 0.054, 0.01, 0.045, 0.07, 0.06, 0.02, 0.002]
+x_points = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+y_points = [0.21, 0.39, 0.51, 0.61, 0.68, 0.72, 0.86, 0.78, 0.83, 1.1]
 # plt.scatter(x_points, y_points, color='#1579ef')
 # 创建垂线的长度
 line_lengths = [0.56, 0.45, 0.30, 0.14, 0.002, 0.09, 0.13, 0.11, 0.054, 0.01, 0.045, 0.07, 0.06, 0.02, 0.002]
 
-# plt.scatter(x_points, y_points, label='给定点', color='blue')
+plt.scatter(x_points, y_points, color='#1579ef')
 
 # for i in range(len(x_points)):
 #    plt.vlines(x = x_points[i], ymin = 0, ymax = line_lengths[i], color='red', linestyle='--')
 
-x0 = np.arange(0, 150, 0.1)
-y0 = 12*PI()*A1/1000000/SQRT(1+(12*PI()*A1/1000000)*(12*PI()*A1/1000000))
-# plt.plot(x0,y0)
+x0 = np.arange(0, 50, 0.1)
+y0 = 12 * np.pi * x0 / 1000 / np.sqrt(1 + (12 * np.pi * x0 / 1000) * (12 * np.pi * x0 / 1000))
+plt.plot(x0,y0)
 
 # =12*PI()*A1/1000000/SQRT(1+(12*PI()*A1/1000000)*(12*PI()*A1/1000000))
 
